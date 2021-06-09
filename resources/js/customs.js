888236ab15f36ce5859705aec4b1fe3b63ee7896
Pranta-biswas <prantabiswas11702@gmail.com>
2021-06-09 17:27:38 +0600

    first commit

---
+++ resources/js/customs.js
@@ -0,0 +1,55 @@
+
+// jQuery hide, show and toggle 
+
+$('#hide').click(function(){
+    $('#p').hide();
+});
+
+$('#show').click(function(){
+    $('#p').show();
+});
+
+$('#toggle').click(function(){
+    $('#p').toggle();
+});
+
+
+
+// jQuery fadeOut, fadeIn, fadeToggle 
+
+
+$('#fadeOut').click(function(){
+    $('#pa').fadeOut();
+});
+
+
+$('#fadeIn').click(function(){
+    $('#pa').fadeIn();
+});
+
+$('#fadeToggle').click(function(){
+    $('#pa').fadeToggle();
+});
+
+
+// jQuery slideUp, slideDown, fadeToggle 
+
+$('#slideUp').click(function(){
+    $('#pag').slideUp();
+});
+
+$('#slideDown').click(function(){
+    $('#pag').slideDown();
+});
+
+$('#slideToggle').click(function(){
+    $('#pag').slideToggle();
+});
+
+/*===========================*/
+
+$('#nkanf').click(function(){
+    $('#addClass').addClass('color');
+});
+
+
